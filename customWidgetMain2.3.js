f350218b434f4e548f3783b4c9f0d63db39cb34c
Nandini914 <152012103+nandiniraynag@users.noreply.github.com>
2025-03-15 19:25:53 +0530

    Create customWidgetMain2.3.js

---
+++ customWidgetMain2.3.js
@@ -0,0 +1,38 @@
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+   / Function to capture the input string from the text box and apply filters
+	 // Function to capture the input string from the text box and apply filters
+		function applyFilters(inputString) {
+   
+    // Define a regular expression to match the key-value pairs
+    const regex = /(\w+\s*\w*)\s*=\s*([^,]+)/g;
+
+    // Initialize an object to store the extracted values
+    var filters = {};
+
+    // Use the regular expression to extract key-value pairs
+    let match;
+    while ((match = regex.exec(inputString)) !== null) {
+        // Extract the key
+        const key = match[1].trim();
+        
+        // Extract the value
+        const value = match[2].trim();
+        
+        // Store the key-value pair in the filters object
+        filters[key] = value;
+        console.log(filters);
+    }
+       // Return the extracted filters
+    		return filters;
+
+  }
+  customElements.define("custom-widget", MainWebComponent);
+})();
